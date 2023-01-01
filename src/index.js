2fd450047fd09ef7523013bbdc5fd0d81fa33464
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-01-01 23:41:34 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import { quotes } from './js/quotes';
 const nextBtn = document.querySelector('.js-next-btn');
 const quoteOriginal = document.querySelector('.quote__text');
 const quoteTranslated = document.querySelector('.quote__translated-text');
+const menuBtn = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
 
 nextBtn.addEventListener('click', generateQuote);
+menuBtn.addEventListener('click', onMenuBtnClick);
 
 function generateQuote() {
   let random = Math.floor(Math.random() * quotes.length);
   quoteOriginal.textContent = quotes[random].quote;
+}
+
+function onMenuBtnClick() {
+  menuBtn.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-active');
 }
 
 generateQuote();
