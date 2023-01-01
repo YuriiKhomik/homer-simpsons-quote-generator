b72fa130a3d242cfc1958c05508221ff99c80400
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-01-01 08:50:52 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const nextBtn = document.querySelector('.js-next-btn');
 const quoteOriginal = document.querySelector('.quote__original-text');
 const quoteTranslated = document.querySelector('.quote__translated-text');
 
-nextBtn.addEventListener('click', onNextBtnClick);
+nextBtn.addEventListener('click', generateQuote);
 
-function onNextBtnClick() {
+function generateQuote() {
   let random = Math.floor(Math.random() * quotes.length);
   quoteOriginal.textContent = quotes[random].quote;
-  quoteTranslated.textContent = quotes[random].translate;
 }
+
+generateQuote();
